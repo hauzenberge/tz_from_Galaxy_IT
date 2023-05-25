a2d9b6b6793915981c207455ec0e59cbabca9a9e
hauzenberge <shapovalik26@gmail.com>
2023-05-25 11:59:25 +0300

    first commit

---
+++ app/Models/Role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = [
+        'name',
+        'guard_name',
+
+    ];
+
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+
+    ];
+
+    protected $appends = ['resource_url'];
+
+    /* ************************ ACCESSOR ************************* */
+
+    public function getResourceUrlAttribute()
+    {
+        return url('/admin/roles/' . $this->getKey());
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(RoleHasPermission::class);
+    }
+    
+    public function permissionDetails()
+    {
+        return $this->hasMany(RoleHasPermission::class)->with('permission');
+    }
+}
---
+++ app/Models/RoleHasPermission.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoleHasPermission extends Model
+{
+    protected $fillable = [
+        'permission_id',
+        'role_id',
+    
+    ];
+    
+    
+    protected $dates = [
+    
+    ];
+    public $timestamps = false;
+    
+    protected $appends = ['resource_url'];
+
+    /* ************************ ACCESSOR ************************* */
+
+    public function getResourceUrlAttribute()
+    {
+        return url('/admin/role-has-permissions/'.$this->getKey());
+    }
+
+    public function permission()
+    {
+        return $this->belongsTo(Permission::class, 'permission_id');
+    }
+}
